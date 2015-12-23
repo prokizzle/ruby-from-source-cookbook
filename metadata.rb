e46da96991b5424ba13ffa2d9629ed28a4f625fb
Nick Prokesch <nprokesch@gmail.com>
2015-12-22 21:55:17 -0600

    Include apt as dependency

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'ruby_from_source'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Nick Prokesch'
+maintainer_email 'nick@prokes.ch'
 license          'All rights reserved'
 description      'Installs/Configures ruby_from_source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
+depends          'apt'

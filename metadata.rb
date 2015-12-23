31898e3cef9afb5932aa93bce6299e8b1c6dc38e
Nick Prokesch <nprokesch@gmail.com>
2015-12-23 15:04:34 -0600

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'nick@prokes.ch'
 license          'All rights reserved'
 description      'Installs/Configures ruby_from_source'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 depends          'apt'
